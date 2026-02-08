47acd5e51ea92eaeb4def7d5bda8a58962548a71
eduardo-g-silva <eduardo@open-ecommerce.org>
2026-02-08 13:09:21 +0000

    Add custom products block with variant support for Box Size attribute

--- __manifest__.py
+++ __manifest__.py
@@ -15,12 +15,14 @@
     "depends": ["website_sale"],
     "data": [
         "views/templates.xml",
+        "views/snippets_products_block.xml",
         "views/res_config_settings_views.xml",
     ],
     "assets": {
         "web.assets_frontend": [
             "chefrulo_website/static/src/scss/style.scss",
             "chefrulo_website/static/src/js/menu_add_to_cart.js",
+            "chefrulo_website/static/src/js/products_block_variants.js",
             "chefrulo_website/static/src/js/frozen_image_modal.js",
         ],
     },
---
+++ static/src/js/products_block_variants.js
@@ -0,0 +1,45 @@
+/** @odoo-module **/
+
+import publicWidget from "@web/legacy/js/public/public_widget";
+import { jsonrpc } from "@web/core/network/rpc_service";
+
+publicWidget.registry.ProductsBlockAddToCart = publicWidget.Widget.extend({
+    selector: '.js_products_block_add_to_cart',
+    events: {
+        'click .js_products_cart_btn': '_onAddToCart',
+    },
+
+    _onAddToCart: function (ev) {
+        ev.preventDefault();
+        var $btn = $(ev.currentTarget);
+        var productId = parseInt($btn.data('product-id'));
+        var $form = $btn.closest('.js_products_block_add_to_cart');
+        var qty = parseInt($form.find('input[name="add_qty"]').val()) || 1;
+
+        $btn.prop('disabled', true);
+        var originalHtml = $btn.html();
+        $btn.html('<i class="fa fa-spinner fa-spin"/>');
+
+        jsonrpc('/shop/cart/update_json', {
+            product_id: productId,
+            add_qty: qty,
+        }).then(function (data) {
+            // Update cart badge in navbar
+            var cartQty = data.cart_quantity || 0;
+            $('sup.my_cart_quantity').text(cartQty).toggle(cartQty > 0);
+            // Show success feedback
+            $btn.html('<i class="fa fa-check me-1"/> Added');
+            $btn.removeClass('btn-primary').addClass('btn-success');
+            setTimeout(function () {
+                $btn.html(originalHtml);
+                $btn.removeClass('btn-success').addClass('btn-primary');
+                $btn.prop('disabled', false);
+            }, 1500);
+        }).catch(function () {
+            $btn.html(originalHtml);
+            $btn.prop('disabled', false);
+        });
+    },
+});
+
+export default publicWidget.registry.ProductsBlockAddToCart;
